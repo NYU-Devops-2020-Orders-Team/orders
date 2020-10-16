fbb4e82ec86195cf94426b28c691123b67f4092d
jiadai <jojo.daijia@gmail.com>
2020-10-16 12:09:41 -0400

    add create order

--- service/models.py
+++ service/models.py
@@ -123,6 +123,19 @@ class Order(db.Model):
             )
         return self
 
+    @app.route('/orders', methods=['POST'])
+    def create_orders():
+        app.logger.info('Create Order requested')
+        order = Order ()
+        order.deserialize(request.get_json())
+        order.save()
+        app.logger.info('Created Order with id: {}'.format(order.id))
+
+        return make_response(jsonify(order.serialize()),
+                             status.HTTP_201_CREATED,
+                             {'Location': url_for('get_orders', order_id=order.id, _external=True)})
+
+
     @classmethod
     def init_db(cls, app):
         """Initializes the database session
