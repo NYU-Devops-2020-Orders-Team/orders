2144467844da7cd0637d82f66d6c8c81919b5f69
jiadai <jojo.daijia@gmail.com>
2020-10-16 12:37:13 -0400

    add function create_orders()

--- service/service.py
+++ service/service.py
@@ -42,3 +42,16 @@ def init_db():
     """ Initialies the SQLAlchemy app """
     global app
     Order.init_db(app)
+
+
+
+@app.route('/orders', methods=['POST'])
+def create_orders():
+    app.logger.info('Create Order requested')
+    order = Order()
+    order.deserialize(request.get_json())
+    order.create()
+    app.logger.info('Created Order with id: {}'.format(order.id))
+    return make_response(jsonify(order.serialize()),
+                         status.HTTP_201_CREATED,
+                         {'Location': url_for('get_orders', order_id=order.id, _external=True)})
