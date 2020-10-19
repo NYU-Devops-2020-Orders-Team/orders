9abba6537d8cba6616f0e8132a915243496700a1
jiadai <jojo.daijia@gmail.com>
2020-10-18 22:07:07 -0400

    add error handlers and optimize create orders

--- service/service.py
+++ service/service.py
@@ -14,8 +14,85 @@ from werkzeug.exceptions import NotFound
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from .models import Order
+from .models import Order,DataValidationError
 from . import app
+
+######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
+
+
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+        ),
+        status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
+        status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            error="Method not Allowed",
+            message=message,
+        ),
+        status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            error="Unsupported media type",
+            message=message,
+        ),
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    )
+
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = str(error)
+    app.logger.error(message)
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=message,
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
 
 
 ######################################################################
@@ -44,16 +121,38 @@ def init_db():
     Order.init_db(app)
 
 
+######################################################################
+# ADD A NEW ORDER
+######################################################################
+
 
 @app.route('/orders', methods=['POST'])
 def create_orders():
-    app.logger.info('Create Order requested')
+    """
+    Creates an order
+    This endpoint will create an order based the data in the body that is posted
+    """
+    app.logger.info("Request to create an order")
+    check_content_type("application/json")
     order = Order()
-    order.deserialize(request.get_json())
 
-    if order.customer_id == "":
-        return make_response(jsonify(order.serialize()),status.HTTP_400_BAD_REQUEST)
+    try:
+        order.deserialize(request.get_json())
+        order.create()
+        message = order.serialize()
+        app.logger.info('Created Order with id: {}'.format(order.id))
+        return make_response(jsonify(order.serialize()),status.HTTP_201_CREATED)
 
-    order.create()
-    app.logger.info('Created Order with id: {}'.format(order.id))
-    return make_response(jsonify(order.serialize()),status.HTTP_201_CREATED)
+    except Exception as e:
+        return request_validation_error(e)
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if request.headers["Content-Type"] == content_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(415, "Content-Type must be {}".format(content_type))
