1dcc4246e23e1771c9f0bf7f87e1c9eb311b6b13
Rutvik Shah <rss638@nyu.edu>
2020-11-11 01:55:20 -0500

    Added skeleton feature file and steps folder for BDD testing

---
+++ features/steps/order_steps.py
@@ -0,0 +1,21 @@
+"""
+Order Steps
+
+Steps file for Order.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+from os import getenv
+import logging
+import json
+import requests
+from behave import *
+from compare import expect, ensure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions
+
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+ID_PREFIX = 'order_'
