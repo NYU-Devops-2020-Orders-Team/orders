3e528c038e5a8cf85a5e1cdadc2fc395a7444d89
jiadai <jojo.daijia@gmail.com>
2020-12-08 07:54:15 -0500

    refactor delete

--- service/service.py
+++ service/service.py
@@ -301,22 +301,24 @@ def get_customer_id_from_request(json):
 
 
 ######################################################################
-# DELETE AN ORDER
+# PATH: /orders/{order_id}/delete
 ######################################################################
-@app.route("/orders/<int:order_id>", methods=["DELETE"])
-def delete_orders(order_id):
-    """
-    Delete an Order
-    This endpoint will delete an Order based the id specified in the path
-    """
-    app.logger.info("Request to delete order with id: %s", order_id)
-    order = Order.find(order_id)
-    if order:
-        order.delete()
+@api.route('/orders/<int:order_id>/delete')
+@api.param('order_id', 'The Order identifier')
+class DeleteOrderResource(Resource):
+    """ Delete actions on an Order """
+    @api.doc('delete_orders')
+    @api.response(404, 'Order not found')
+    @api.marshal_with(order_model)
+    def put(self, order_id):
+        """ Delete all the items of the Order """
+        app.logger.info("Request to delete order with id: %s", order_id)
+        order = Order.find(order_id)
+        if order:
+            order.delete()
 
-    app.logger.info("Order with ID [%s] delete complete.", order_id)
-    return make_response("", status.HTTP_204_NO_CONTENT)
-
+        app.logger.info("Order with ID [%s] delete complete.", order_id)
+        return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 # PATH: /orders/{order_id}/cancel
